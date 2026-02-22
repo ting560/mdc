7653114841d1e406ad5c9feda6d6cd0bde2cfc40
ting560 <160437906+ting560@users.noreply.github.com>
2026-02-22 12:42:13 -0300

    Fix error message and add cache control header

--- api/proxy.js
+++ api/proxy.js
@@ -1,13 +1,15 @@
 const http = require('http');
 
 export default function handler(req, res) {
-  const url = 'http://live.sinalmycn.com/11000/mpegts'; // O link que deu erro
+  // O link HTTP que o navegador estava bloqueando
+  const url = 'http://live.sinalmycn.com/11000/mpegts';
 
   http.get(url, (response) => {
     res.setHeader('Content-Type', 'video/mp2t');
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Cache-Control', 'no-cache');
     response.pipe(res);
   }).on('error', (e) => {
-    res.status(500).send("Erro no stream: " + e.message);
+    res.status(500).send("Erro ao carregar stream: " + e.message);
   });
 }
